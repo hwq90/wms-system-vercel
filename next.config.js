5cb7f5b648e07d014ee74de21499e17c966098f7
hwq90 <78943806@qq.com>
2026-04-11 20:04:33 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const nextConfig = {
   reactStrictMode: false,
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  experimental: { turbo: false }
 }
 
 module.exports = nextConfig
